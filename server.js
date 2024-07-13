41caf92d0d69b6d94abca9ac812b36c66cfde1ad
Obinabo Walter <73674818+Obinabo@users.noreply.github.com>
2024-07-13 22:03:03 +0100

    some commit
    no desc.

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const http = require('http');
-const fs = require('fs'); // Re-added this line
-const cron = require('node-cron');
-
+const fs = require('fs');
 const app = express();
+
+const cron = require('node-cron');
 
 var corsOptions = {
   // origin: "https://...app"
 };
 
 app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+// parse requests of content-type - application/json
+app.use(express.json());  /* bodyParser.json() is deprecated */
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));   /* bodyParser.urlencoded() is deprecated */
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome." });
@@ -20,7 +24,7 @@ app.get("/", (req, res) => {
 
 require("./app/routes/routes")(app);
 
+// set port, listen for requests
 const PORT = process.env.PORT || 8000;
-http.createServer(app).listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+http.createServer(app).listen(PORT);
+console.log(`Server is running on port ${PORT}.`);
